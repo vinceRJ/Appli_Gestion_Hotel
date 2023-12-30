8f7ce0aa148a34bea0a55eb4e185aa44aa881769
rjvince <jephte-vince-realite.tokanou@ens.uvsq.fr>
2023-12-30 13:22:27 +0100

    ajout du controller

---
+++ controller/HotelController.java
@@ -0,0 +1,19 @@
+// Classe responsable de la coordination
+
+package controller;
+
+import model.Client;
+import model.Chambre;
+import model.Reservation;
+import model.Repas;
+import model.Facture;
+
+public class HotelController {
+     // on va  inclure des références aux différentes parties du modèle ici
+
+     public HotelController() {
+        // Initialisation si nécessaire
+    }
+    
+
+}
--- model/ChambreNormale.java
+++ model/ChambreNormale.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 public class ChambreNormale extends Chambre implements Reservable {
 
+    private String[] servicesNormal;
     // Constructeur, getters, setters
 
     // Implémentation des méthodes de l'interface Reservable
